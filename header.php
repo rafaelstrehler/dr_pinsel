8f19df01a6a68e071149219f2e55164965b3afdf
André Schmid <andre.schmid@08eins.ch>
2018-01-23 14:08:41 +0100

    mobile menu working

--- header.php
+++ header.php
@@ -22,17 +22,31 @@
   <div class="container">
     <div class="page-header">
       <img src="<?= get_template_directory_uri(); ?>/assets/images/aerztekunstlogo.PNG">
-      <?php
-      wp_nav_menu( array(
-          'menu' => 'menu1',
-          'depth'             => 2,
-          'container'         => 'div',
-          'container_class'   => 'collapse navbar-collapse',
-          'container_id'      => 'bs-example-navbar-collapse-1',
-          'menu_class'        => 'nav navbar-nav navbar-inverse',
-          'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-          'walker'            => new WP_Bootstrap_Navwalker(),
-      ) );
-      ?>
+      <nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+        		<span class="icon-bar"></span>
+        		<span class="icon-bar"></span>
+        		<span class="icon-bar"></span>
+			</button>
+		</div>
+    <?php
+    wp_nav_menu( array(
+        'menu' => 'menu1',
+        'depth'             => 2,
+        'container'         => 'div',
+        'container_class'   => 'collapse navbar-collapse',
+        'container_id'      => 'bs-example-navbar-collapse-1',
+        'menu_class'        => 'nav navbar-nav',
+        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+        'walker'            => new WP_Bootstrap_Navwalker(),
+    ) );
+    ?>
+    </div>
+</nav>
+
     </div>
   </div>
